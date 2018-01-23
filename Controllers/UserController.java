63a51155ceaa8fe6523c23f3b263eeef299f73a3
Czarsiak <czarek6000@interia.pl>
2018-01-23 21:29:40 +0100

    add annotation @CrossOrigin

--- Controllers/UserController.java
+++ Controllers/UserController.java
@@ -6,6 +6,7 @@ import pl.czarsiak.demo.Model.User;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/users")
 public class UserController {
